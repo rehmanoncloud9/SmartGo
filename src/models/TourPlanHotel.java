dd1a2cc182f5b34967aeb886814b435ce98ddee3
Your Name <you@example.com>
2026-04-25 23:03:31 +0500

    Add TourPlanHotel to link a tour plan with its default hotel

---
+++ src/models/TourPlanHotel.java
@@ -0,0 +1,30 @@
+package models;
+
+// This links a tour plan to a hotel.
+// isDefault means this is the main recommended hotel for that tour plan.
+
+public class TourPlanHotel {
+
+    private int id;
+    private int tourPlanId;
+    private int hotelId;
+    private boolean isDefault;
+
+    public TourPlanHotel(int id, int tourPlanId, int hotelId, boolean isDefault) {
+        this.id = id;
+        this.tourPlanId = tourPlanId;
+        this.hotelId = hotelId;
+        this.isDefault = isDefault;
+    }
+
+    public int getId() { return id; }
+    public int getTourPlanId() { return tourPlanId; }
+    public int getHotelId() { return hotelId; }
+    public boolean isDefault() { return isDefault; }
+
+    @Override
+    public String toString() {
+        return "TourPlanHotel ID: " + id + " | Tour Plan: " + tourPlanId
+                + " | Hotel: " + hotelId + " | Default: " + isDefault;
+    }
+}
